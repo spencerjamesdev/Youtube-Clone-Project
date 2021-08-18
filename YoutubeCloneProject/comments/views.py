15d076b3b64b6a1cf34497e63624ce355a1769a0
spencerjamesdev <spencer.franke@gmail.com>
2021-08-18 15:32:21 -0500

    Added django REST API Framework up to page 12 of the tutroial worksheet

--- YoutubeCloneProject/comments/views.py
+++ YoutubeCloneProject/comments/views.py
@@ -1,3 +1,16 @@
+from YoutubeCloneProject.comments.serializers import CommentSerializer
 from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class CommentList(APIView):
+
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
