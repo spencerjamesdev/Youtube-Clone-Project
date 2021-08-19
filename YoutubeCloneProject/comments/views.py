588f1c19ebec0810534f4686d51474d8dd3825f5
TBuscemi <thomasbuscemi124@gmail.com>
2021-08-19 14:30:37 -0400

    Made the paths to the comments and built the views needed for post man to work witrh comments.

--- YoutubeCloneProject/comments/views.py
+++ YoutubeCloneProject/comments/views.py
@@ -1,4 +1,3 @@
-from YoutubeCloneProject.comments.serializers import CommentSerializer
 from django.shortcuts import render
 from .models import Comment
 from .serializers import CommentSerializer
@@ -14,3 +13,37 @@ class CommentList(APIView):
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400__BAD_REQUEST)
+
+
+class Detail(APIView):
+
+    def get_comments(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            return Response (status = 404)
+
+    def get(self, request, pk):
+        comments = self.get_comments(pk=pk)
+        serializer = CommentSerializer(comments)
+        return Response(serializer.data)
+
+    def put(self,request, pk):
+        comments = self.get_comments(pk=pk)
+        serializer = CommentSerializer(comments, data=request.data)
+        if  serializer.is_valid():    
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk):
+            comments = self.get_comments(pk=pk)
+            comments.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
